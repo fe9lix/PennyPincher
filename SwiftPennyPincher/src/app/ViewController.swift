cb8b54c09125b3078aad9a7c8aec69edfe8cdc1b
fe9lix <fe9lix@googlemail.com>
2015-07-19 15:12:01 +0200

    Add gesture view with path and sampling point drawing.

--- SwiftPennyPincher/src/app/ViewController.swift
+++ SwiftPennyPincher/src/app/ViewController.swift
@@ -1,25 +1,13 @@
-//
-//  ViewController.swift
-//  SwiftPennyPincher
-//
-//  Created by Felix on 19.07.15.
-//  Copyright (c) 2015 betriebsraum. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gestureView: GestureView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        gestureView.showSamplingPoints = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
